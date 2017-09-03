025279104909a22d2bc595c17fc266ea9b980c61
khanhkiem <buidoigiauco@gmail.com>
2017-09-03 20:55:16 +0700

    Kaithy can do valid random move now

--- main.py
+++ main.py
@@ -3,19 +3,24 @@ import json
 import numpy as np
 
 
-def read_input():
+def read_input() -> str:
     lines = sys.stdin.readlines()
     # Since our input would only be having one line, parse our JSON data from that
     return json.loads(lines[0])
 
 
 def main():
-    # get our data as an array from read_in()
-    input = read_input()
+    my_input = read_input()
+    num_empty_square = my_input.count('2')
+    empty_move_idx = np.random.randint(num_empty_square)
 
-    # return the sum to the output stream
-    output = "3"
-    print(output)
+    empty_square_count = -1
+    for square_idx in range(0, len(my_input)):
+        empty_square_count += (my_input[square_idx] == '2')
+        if empty_square_count == empty_move_idx:
+            my_output = square_idx
+
+    print(my_output)
 
 
 if __name__ == "__main__":
