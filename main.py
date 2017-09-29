c2d769c0430dce71c70b1360fafb7a429d4dd798
khanhkiem <buidoigiauco@gmail.com>
2017-09-30 00:08:06 +0700

    Fix bug: forget to break when located random move

--- main.py
+++ main.py
@@ -10,15 +10,19 @@ def read_input() -> str:
 
 
 def main():
+    empty = '0'
+
     my_input = read_input()
-    num_empty_square = my_input.count('2')
+    num_empty_square = my_input.count(empty)
     empty_move_idx = np.random.randint(num_empty_square)
 
     empty_square_count = -1
     for square_idx in range(0, len(my_input)):
-        empty_square_count += (my_input[square_idx] == '2')
+        empty_square_count += (my_input[square_idx] == empty)
+        print(empty_square_count)
         if empty_square_count == empty_move_idx:
             my_output = square_idx
+            break
 
     print(my_output)
 
