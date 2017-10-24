e56eda3e29a5e08bfddef41abc724626919d44ed
khanhkiem <buidoigiauco@gmail.com>
2017-10-24 19:30:17 +0700

    Refactor

--- adversarial_gym/gym_gomoku/envs/gomoku.py
+++ adversarial_gym/gym_gomoku/envs/gomoku.py
@@ -378,5 +378,5 @@ class Board(object):
         '''Return: np array
             np.array(board_size, board_size): state observation of the board
         '''
-        img = np.array(self.board_state)  # shape [board_size, board_size]
-        return img
+        # shape [board_size, board_size]
+        return self.board_state
