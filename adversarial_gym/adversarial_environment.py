110ad4a1c8fe547ea751e68ffd8aef8a34226aa8
khanhkiem <buidoigiauco@gmail.com>
2017-10-27 14:51:40 +0700

    public __opponent_policy --> opponent_policy

--- adversarial_gym/adversarial_environment.py
+++ adversarial_gym/adversarial_environment.py
@@ -9,12 +9,12 @@ from . import gym_gomoku
 class AdversarialEnv:
     def __init__(self, environment_id, opponent_policy):
         self.env = gym.make(environment_id)
-        self.__opponent_policy = opponent_policy
+        self.opponent_policy = opponent_policy
 
         self.reset()
 
     def set_opponent_policy(self, opponent_policy):
-        self.__opponent_policy = opponent_policy
+        self.opponent_policy = opponent_policy
 
     def step(self, action):
         return self.env.step(action)
@@ -22,7 +22,7 @@ class AdversarialEnv:
     def reset(self):
         # A trick
         # Reset env and attach defined opponent policy to env
-        return self.env.step(self.__opponent_policy)
+        return self.env.step(self.opponent_policy)
 
     def render(self):
         return self.env.render()
