a0c2288e93e8ec113571e0bdef1b91a6ba1b33b4
khanhkiem <buidoigiauco@gmail.com>
2017-10-30 19:09:59 +0700

    Invert board

--- test/test_invert_board.py
+++ test/test_invert_board.py
@@ -3,14 +3,24 @@ sys.path.append('..')
 
 import adversarial_gym as gym
 import numpy as np
-import util
+import copy
 
 
 def opponent_policy(curr_state, prev_state, prev_action):
     '''
     Define policy for opponent here
     '''
-    print(curr_state)
+    print(curr_state.board.board_state)
+    inverted_board = np.copy(curr_state.board.board_state)
+    for board_space in np.nditer(inverted_board, op_flags=['readwrite']):
+        if board_space == 1:
+            # board_space is read only
+            # to assign new_value, use board_space[...] = new_value
+            board_space[...] = 2
+        elif board_space == 2:
+            board_space[...] = 1
+    print(inverted_board)
+
     return gym.gym_gomoku.envs.util.make_beginner_policy(np.random)(curr_state, prev_state, prev_action)
 
 
@@ -20,9 +30,9 @@ def main():
     '''
     env = gym.make('Gomoku9x9-training-camp-v0', opponent_policy)
     env.reset()
+
     action = env.action_space.sample()  # sample without replacement
     observation, reward, done, info = env.step(action)
-    # util.invert_board(observation)
 
 
 if __name__ == "__main__":
