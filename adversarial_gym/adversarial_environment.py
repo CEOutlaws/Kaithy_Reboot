38f6627dd55eb96314ad20c404dcbc6399c4d97e
khanhkiem <buidoigiauco@gmail.com>
2017-11-02 15:02:23 +0700

    add swap and refactor

--- adversarial_gym/adversarial_environment.py
+++ adversarial_gym/adversarial_environment.py
@@ -8,34 +8,40 @@ from . import gym_gomoku
 
 class AdversarialEnv:
     def __init__(self, environment_id, opponent_policy=None):
-        self.env = gym.make(environment_id)
-        self._opponent_policy = opponent_policy
+        self.__env = gym.make(environment_id)
+        self.__opponent_policy = opponent_policy
 
         self.reset()
 
     @property
     def opponent_policy(self):
-        return self._opponent_policy
+        return self.__opponent_policy
 
     @opponent_policy.setter
     def opponent_policy(self, opponent_policy):
-        self._opponent_policy = opponent_policy
+        self.__opponent_policy = opponent_policy
+
+    def swap_role(self):
+        if (self.__env.player_color == 'black'):
+            self.__env.player_color = 'white'
+        else:
+            self.__env.player_color = 'black'
 
     def step(self, action):
-        return self.env.step(action)
+        return self.__env.step(action)
 
     def reset(self):
         # A trick
         # Reset env and attach defined opponent policy to env
-        return self.env.step(self._opponent_policy)
+        return self.__env.step(self.__opponent_policy)
 
     def render(self):
-        return self.env.render()
+        return self.__env.render()
 
     @property
     def action_space(self):
-        return self.env.action_space
+        return self.__env.action_space
 
     @property
     def observation_space(self):
-        return self.env.observation_space
+        return self.__env.observation_space
