5de72f8451c594ccfb5e1f96a49b52411ada8522
khanhkiem <buidoigiauco@gmail.com>
2017-11-02 23:25:00 +0700

    Success fine min q value for each entry

--- test/test_filter_valid_action.py
+++ test/test_filter_valid_action.py
@@ -18,10 +18,23 @@ def main():
     AI Self-training program
     '''
     env = gym.make('Gomoku5x5-training-camp-v0', opponent_policy)
+    q_values_random = (
+        (1.2, 2),
+        (2.5, -2),
+        (1, -3)
+    )
+
     obs_ph = tf.placeholder(
         dtype=tf.float32, shape=[None] + list(env.observation_space.shape))
     invalid_mask_ph = tf.placeholder(
         dtype=tf.bool, shape=[None] + list(env.action_space.invalid_mask.shape))
+    # Compute q_values
+    q_values = tf.constant(q_values_random, dtype=tf.float32)
+    # Get min
+    q_values_worst = tf.reduce_min(q_values, axis=1)
+    # Assign q_value = min q_value if invalid action,before find action that give max q_value
+    # q_values = tf.where()
+    deterministic_actions = tf.argmax(q_values, axis=1)
     sess = tf.Session()
     for i in range(2):
         observation = env.reset()
@@ -32,8 +45,8 @@ def main():
             observation, reward, done, info = env.step(action)
             env.render()
 
-            print(sess.run(obs_ph, feed_dict={obs_ph: observation[None]}))
-            print(sess.run(invalid_mask_ph, feed_dict={
+            print(sess.run(q_values_worst, feed_dict={
+                  obs_ph: observation[None],
                   invalid_mask_ph: env.action_space.invalid_mask[None]}))
 
         env.swap_role()
