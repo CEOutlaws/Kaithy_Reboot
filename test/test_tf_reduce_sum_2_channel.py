451d424830804ddb0f703c641d8b1bc3d36fb9bc
khanhkiem <buidoigiauco@gmail.com>
2017-11-06 12:42:00 +0700

    sum 2 first  channel  only

--- test/test_tf_reduce_sum_2_channel.py
+++ test/test_tf_reduce_sum_2_channel.py
@@ -6,15 +6,16 @@ def main():
         [
             [[1, 0, 0], [0, 1, 0], [0, 1, 0]],
             [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
-            [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+            [[0, 0, 0], [0, 1, 0], [0, 0, 0]]
         ],
         [
             [[1, 0, 1], [0, 1, 1], [0, 1, 1]],
             [[0, 0, 1], [0, 0, 1], [0, 0, 1]],
-            [[0, 0, 1], [0, 0, 1], [0, 0, 1]]
+            [[1, 0, 1], [0, 0, 1], [0, 0, 1]]
         ],
     ], dtype=tf.float32)
-    output = tf.reduce_sum(input[:, :, :, 0:2], axis=3, keep_dims=True)
+    output = tf.contrib.layers.flatten(
+        tf.reduce_sum(input[:, :, :, 0:2], axis=3))
     sess = tf.Session()
     print(sess.run(output))
 
