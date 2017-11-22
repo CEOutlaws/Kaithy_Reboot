6ed5c4efcfc9e782694c4796e44555bbad05e77f
khanhkiem <buidoigiauco@gmail.com>
2017-11-22 13:42:08 +0700

    add test arena

---
+++ test/test_arena.py
@@ -0,0 +1,37 @@
+import sys
+sys.path.append('..')
+
+import adversarial_gym as gym
+import numpy as np
+import copy
+
+
+def opponent_policy(curr_state, prev_state, prev_action):
+    '''
+    Define policy for opponent here
+    '''
+    return gym.gym_gomoku.envs.util.make_beginner_policy(np.random)(curr_state, prev_state, prev_action)
+
+
+def main():
+    '''
+    AI Self-training program
+    '''
+    env = gym.make('Gomoku5x5-arena-v0', opponent_policy)
+
+    for i in range(2):
+        observation = env.reset()
+        done = None
+
+        while not done:
+            action = env.action_space.sample()  # sample without replacement
+            observation, reward, done, info = env.step(action)
+            print(observation)
+            env.render()
+
+        env.swap_role()
+        print("\n----SWAP----\n")
+
+
+if __name__ == "__main__":
+    main()
