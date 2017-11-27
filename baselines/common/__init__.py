7a458c2bc4ee63a89e94f1a0b7434ef14559444d
khanhkiem <buidoigiauco@gmail.com>
2017-11-27 18:21:42 +0700

    transform data

--- baselines/common/__init__.py
+++ baselines/common/__init__.py
@@ -3,3 +3,4 @@ from baselines.common.dataset import Dataset
 from baselines.common.math_util import *
 from baselines.common.misc_util import *
 from baselines.common.position import *
+from baselines.common.transformer import *
---
+++ baselines/common/transformer.py
@@ -0,0 +1,47 @@
+import tensorflow as tf
+from tensorflow import image
+
+import baselines.common.position as pos
+
+
+def transform_actions(actions, board_size):
+    batch_size = tf.shape(actions)[0]
+
+    return tf.concat((
+        actions[0: batch_size // 8],
+        pos.rot90(
+            board_size, actions[batch_size // 8: batch_size // 4], 1),
+        pos.rot90(
+            board_size, actions[batch_size // 4: batch_size // 8 * 3], 2),
+        pos.rot90(
+            board_size, actions[batch_size // 8 * 3: batch_size // 2], 3),
+        pos.flip_left_right_rot90(
+            board_size, actions[batch_size // 2: batch_size // 8 * 5], 0),
+        pos.flip_left_right_rot90(
+            board_size, actions[batch_size // 8 * 5: batch_size // 8 * 6], 1),
+        pos.flip_left_right_rot90(
+            board_size, actions[batch_size // 8 * 6: batch_size // 8 * 7], 2),
+        pos.flip_left_right_rot90(
+            board_size, actions[batch_size // 8 * 7: batch_size], 3),
+    ), axis=0)
+
+
+def transform_obses(obses):
+    batch_size = tf.shape(obses)[0]
+    return tf.concat((
+        obses[0: batch_size // 8],
+        tf.map_fn(lambda obs: image.rot90(obs, 1),
+                  obses[batch_size // 8: batch_size // 4]),
+        tf.map_fn(lambda obs: image.rot90(obs, 2),
+                  obses[batch_size // 4: batch_size // 8 * 3]),
+        tf.map_fn(lambda obs: image.rot90(obs, 3),
+                  obses[batch_size // 8 * 3: batch_size // 2]),
+        tf.map_fn(lambda obs: image.flip_left_right(obs),
+                  obses[batch_size // 2: batch_size // 8 * 5]),
+        tf.map_fn(lambda obs: image.rot90(image.flip_left_right(obs), 1),
+                  obses[batch_size // 8 * 5: batch_size // 8 * 6]),
+        tf.map_fn(lambda obs: image.rot90(image.flip_left_right(obs), 2),
+                  obses[batch_size // 8 * 6: batch_size // 8 * 7]),
+        tf.map_fn(lambda obs: image.rot90(image.flip_left_right(obs), 3),
+                  obses[batch_size // 8 * 7: batch_size]),
+    ), axis=0)
