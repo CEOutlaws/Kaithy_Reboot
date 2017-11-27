34984475c81abcf76ca25c5aaf1061a720af0cdf
khanhkiem <buidoigiauco@gmail.com>
2017-11-27 18:03:07 +0700

    finissh transform obs

--- test/test_transform_obs.py
+++ test/test_transform_obs.py
@@ -7,44 +7,45 @@ import numpy as np
 
 
 def main():
-    board_size = 5
     # Build graph
-    obs_t_input = tf.placeholder(tf.int32, [None, 5, 5, 3], name="action")
+    obs_t_input = tf.placeholder(tf.int32, [None, 5, 5, 1], name="action")
     batch_size = tf.shape(obs_t_input)[0]
     obs_t_aug = tf.concat((
         obs_t_input[0: batch_size // 8],
-        image.rot90(
-            obs_t_input[batch_size // 8: batch_size // 4], 1),
-        image.rot90(
-            obs_t_input[batch_size // 4: batch_size // 8 * 3], 2),
-        image.rot90(
-            obs_t_input[batch_size // 8 * 3: batch_size // 2], 3),
-        image.flip_left_right(
-            obs_t_input[batch_size // 2: batch_size // 8 * 5]),
-        image.rot90(
-            image.flip_left_right(obs_t_input[batch_size // 8 * 5: batch_size // 8 * 6]), 1),
-        image.rot90(
-            image.flip_left_right(obs_t_input[batch_size // 8 * 6: batch_size // 8 * 7]), 2),
-        image.rot90(
-            image.flip_left_right(obs_t_input[batch_size // 8 * 7: batch_size]), 3),
+        tf.map_fn(lambda obs: image.rot90(obs, 1),
+                  obs_t_input[batch_size // 8: batch_size // 4]),
+        tf.map_fn(lambda obs: image.rot90(obs, 2),
+                  obs_t_input[batch_size // 4: batch_size // 8 * 3]),
+        tf.map_fn(lambda obs: image.rot90(obs, 3),
+                  obs_t_input[batch_size // 8 * 3: batch_size // 2]),
+        tf.map_fn(lambda obs: image.flip_left_right(obs),
+                  obs_t_input[batch_size // 2: batch_size // 8 * 5]),
+        tf.map_fn(lambda obs: image.rot90(image.flip_left_right(obs), 1),
+                  obs_t_input[batch_size // 8 * 5: batch_size // 8 * 6]),
+        tf.map_fn(lambda obs: image.rot90(image.flip_left_right(obs), 2),
+                  obs_t_input[batch_size // 8 * 6: batch_size // 8 * 7]),
+        tf.map_fn(lambda obs: image.rot90(image.flip_left_right(obs), 3),
+                  obs_t_input[batch_size // 8 * 7: batch_size]),
     ), axis=0)
 
     # Run graph
     sess = tf.Session()
 
     obs_to_test = np.zeros((5, 5, 1), np.int32)
-    obses_to_test = np.array([obs_to_test] * 16)
+    obses_to_test = np.array([obs_to_test] * 25)
 
     for obs_idx in range(len(obses_to_test)):
         obses_to_test[obs_idx][obs_idx // 5, obs_idx % 5][0] = 1
 
-    obs_result = sess.run(obs_t_input, feed_dict={
-                          obs_t_input: obses_to_test})
-    print(obs_result)
+    obses_input = sess.run(obs_t_input, feed_dict={
+        obs_t_input: obses_to_test})
 
-    obs_result = sess.run(obs_t_aug, feed_dict={
-                          obs_t_input: obses_to_test})
-    print(obs_result)
+    obses_result = sess.run(obs_t_aug, feed_dict={
+        obs_t_input: obses_to_test})
+    for obs_idx in range(len(obses_result)):
+        print(obs_idx)
+        print(np.squeeze(obses_input[obs_idx]))
+        print(np.squeeze(obses_result[obs_idx]))
 
 
 if __name__ == "__main__":
